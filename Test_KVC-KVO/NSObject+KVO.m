598e8e6a20defd024405c512869a74e0d378d26e
李巍 <LWnoheart@LWnoheartMacBook-Pro.local>
2017-03-29 17:02:21 +0800

    add KVO basic principles

---
+++ Test_KVC-KVO/NSObject+KVO.m
@@ -0,0 +1,259 @@
+//
+//  NSObject+KVO.m
+//  ImplementKVO
+//
+//  Created by Peng Gu on 2/26/15.
+//  Copyright (c) 2015 Peng Gu. All rights reserved.
+//
+
+#import "NSObject+KVO.h"
+#import <objc/runtime.h>
+#import <objc/message.h>
+
+NSString *const kPGKVOClassPrefix = @"PGKVOClassPrefix_";
+NSString *const kPGKVOAssociatedObservers = @"PGKVOAssociatedObservers";
+
+
+#pragma mark - PGObservationInfo
+@interface PGObservationInfo : NSObject
+
+@property (nonatomic, weak) NSObject *observer;
+@property (nonatomic, copy) NSString *key;
+@property (nonatomic, copy) PGObservingBlock block;
+
+@end
+
+@implementation PGObservationInfo
+
+- (instancetype)initWithObserver:(NSObject *)observer Key:(NSString *)key block:(PGObservingBlock)block
+{
+    self = [super init];
+    if (self) {
+        _observer = observer;
+        _key = key;
+        _block = block;
+    }
+    return self;
+}
+
+@end
+
+
+#pragma mark - Debug Help Methods
+static NSArray *ClassMethodNames(Class c)
+{
+    NSMutableArray *array = [NSMutableArray array];
+    
+    unsigned int methodCount = 0;
+    Method *methodList = class_copyMethodList(c, &methodCount);
+    unsigned int i;
+    for(i = 0; i < methodCount; i++) {
+        [array addObject: NSStringFromSelector(method_getName(methodList[i]))];
+    }
+    free(methodList);
+    
+    return array;
+}
+
+
+static void PrintDescription(NSString *name, id obj)
+{
+    NSString *str = [NSString stringWithFormat:
+                     @"%@: %@\n\tNSObject class %s\n\tRuntime class %s\n\timplements methods <%@>\n\n",
+                     name,
+                     obj,
+                     class_getName([obj class]),
+                     class_getName(object_getClass(obj)),
+                     [ClassMethodNames(object_getClass(obj)) componentsJoinedByString:@", "]];
+    printf("%s\n", [str UTF8String]);
+}
+
+
+#pragma mark - Helpers
+static NSString * getterForSetter(NSString *setter)
+{
+    if (setter.length <=0 || ![setter hasPrefix:@"set"] || ![setter hasSuffix:@":"]) {
+        return nil;
+    }
+    
+    // remove 'set' at the begining and ':' at the end
+    NSRange range = NSMakeRange(3, setter.length - 4);
+    NSString *key = [setter substringWithRange:range];
+    
+    // lower case the first letter
+    NSString *firstLetter = [[key substringToIndex:1] lowercaseString];
+    key = [key stringByReplacingCharactersInRange:NSMakeRange(0, 1)
+                                       withString:firstLetter];
+    
+    return key;
+}
+
+
+static NSString * setterForGetter(NSString *getter)
+{
+    if (getter.length <= 0) {
+        return nil;
+    }
+    
+    // upper case the first letter
+    NSString *firstLetter = [[getter substringToIndex:1] uppercaseString];
+    NSString *remainingLetters = [getter substringFromIndex:1];
+    
+    // add 'set' at the begining and ':' at the end
+    NSString *setter = [NSString stringWithFormat:@"set%@%@:", firstLetter, remainingLetters];
+    
+    return setter;
+}
+
+
+#pragma mark - Overridden Methods
+static void kvo_setter(id self, SEL _cmd, id newValue)//重写 setter 方法。新的 setter 在调用原 setter 方法后，通知每个观察者（调用之前传入的 block ）我们对 objc_msgSendSuper 进行类型转换。在 Xcode 6 里，新的 LLVM 会对 objc_msgSendSuper 以及 objc_msgSend 做严格的类型检查，如果不做类型转换。Xcode 会抱怨有 too many arguments 的错误。
+{
+    NSString *setterName = NSStringFromSelector(_cmd);
+    NSString *getterName = getterForSetter(setterName);
+    
+    if (!getterName) {
+        NSString *reason = [NSString stringWithFormat:@"Object %@ does not have setter %@", self, setterName];
+        @throw [NSException exceptionWithName:NSInvalidArgumentException
+                                       reason:reason
+                                     userInfo:nil];
+        return;
+    }
+    
+    id oldValue = [self valueForKey:getterName];
+    
+    struct objc_super superclazz = {
+        .receiver = self,
+        .super_class = class_getSuperclass(object_getClass(self))
+    };
+    
+    // cast our pointer so the compiler won't complain
+    void (*objc_msgSendSuperCasted)(void *, SEL, id) = (void *)objc_msgSendSuper;
+    
+    // call super's setter, which is original class's setter method
+    objc_msgSendSuperCasted(&superclazz, _cmd, newValue);
+    
+    // look up observers and call the blocks
+    NSMutableArray *observers = objc_getAssociatedObject(self, (__bridge const void *)(kPGKVOAssociatedObservers));
+    for (PGObservationInfo *each in observers) {
+        if ([each.key isEqualToString:getterName]) {
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                each.block(self, getterName, oldValue, newValue);
+            });
+        }
+    }
+}
+
+
+static Class kvo_class(id self, SEL _cmd)
+{
+    return class_getSuperclass(object_getClass(self));
+}
+
+
+#pragma mark - KVO Category
+@implementation NSObject (KVO)
+
+- (void)PG_addObserver:(NSObject *)observer
+                forKey:(NSString *)key
+             withBlock:(PGObservingBlock)block
+{
+    SEL setterSelector = NSSelectorFromString(setterForGetter(key));//根据key拼装方法名
+    Method setterMethod = class_getInstanceMethod([self class], setterSelector);//获得方法实现
+    if (!setterMethod) {//没有实现抛出异常
+        NSString *reason = [NSString stringWithFormat:@"Object %@ does not have a setter for key %@", self, key];
+        @throw [NSException exceptionWithName:NSInvalidArgumentException
+                                       reason:reason
+                                     userInfo:nil];
+        
+        return;
+    }
+    
+    Class clazz = object_getClass(self);//获得本类isa指针
+    NSString *clazzName = NSStringFromClass(clazz);
+    
+    // if not an KVO class yet
+    if (![clazzName hasPrefix:kPGKVOClassPrefix]) {//如果本类目前还不是监听派生类则动态生成监听类并让本类isa指针指向新派生类
+        clazz = [self makeKvoClassWithOriginalClassName:clazzName];
+        object_setClass(self, clazz);
+    }
+    
+    // add our kvo setter if this class (not superclasses) doesn't implement the setter?
+    if (![self hasSelector:setterSelector]) {
+        const char *types = method_getTypeEncoding(setterMethod);
+        class_addMethod(clazz, setterSelector, (IMP)kvo_setter, types);
+    }
+    
+    PGObservationInfo *info = [[PGObservationInfo alloc] initWithObserver:observer Key:key block:block];
+    NSMutableArray *observers = objc_getAssociatedObject(self, (__bridge const void *)(kPGKVOAssociatedObservers));
+    if (!observers) {
+        observers = [NSMutableArray array];
+        objc_setAssociatedObject(self, (__bridge const void *)(kPGKVOAssociatedObservers), observers, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    [observers addObject:info];
+}
+
+
+- (void)PG_removeObserver:(NSObject *)observer forKey:(NSString *)key
+{
+    NSMutableArray* observers = objc_getAssociatedObject(self, (__bridge const void *)(kPGKVOAssociatedObservers));
+    
+    PGObservationInfo *infoToRemove;
+    for (PGObservationInfo* info in observers) {
+        if (info.observer == observer && [info.key isEqual:key]) {
+            infoToRemove = info;
+            break;
+        }
+    }
+    
+    [observers removeObject:infoToRemove];
+}
+
+
+- (Class)makeKvoClassWithOriginalClassName:(NSString *)originalClazzName
+{
+    NSString *kvoClazzName = [kPGKVOClassPrefix stringByAppendingString:originalClazzName];
+    Class clazz = NSClassFromString(kvoClazzName);
+    
+    if (clazz) {
+        return clazz;
+    }
+    
+    // class doesn't exist yet, make it
+    Class originalClazz = object_getClass(self);
+    Class kvoClazz = objc_allocateClassPair(originalClazz, kvoClazzName.UTF8String, 0);//定义新子类，0表示额外空间为0
+    
+    // grab class method's signature so we can borrow it
+    Method clazzMethod = class_getInstanceMethod(originalClazz, @selector(class));//重写class方法隐藏派生类
+    const char *types = method_getTypeEncoding(clazzMethod);
+    class_addMethod(kvoClazz, @selector(class), (IMP)kvo_class, types);
+    
+    objc_registerClassPair(kvoClazz);//告诉 Runtime 这个类的存在
+    
+    return kvoClazz;
+}
+
+
+- (BOOL)hasSelector:(SEL)selector
+{
+    Class clazz = object_getClass(self);
+    unsigned int methodCount = 0;
+    Method* methodList = class_copyMethodList(clazz, &methodCount);
+    for (unsigned int i = 0; i < methodCount; i++) {
+        SEL thisSelector = method_getName(methodList[i]);
+        if (thisSelector == selector) {
+            free(methodList);
+            return YES;
+        }
+    }
+    
+    free(methodList);
+    return NO;
+}
+
+
+@end
+
+
+
+
